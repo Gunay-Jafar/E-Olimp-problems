17113595a9979c513889b6172203e7ee5e435c0f
Gunay-Jafar <cafarligunay06@gmail.com>
2020-08-02 15:02:30 +0400

    Created Task901

--- src/E_Olymp/Task901.java
+++ src/E_Olymp/Task901.java
@@ -1,4 +1,19 @@
 package E_Olymp;
 
+import java.util.Scanner;
+
 public class Task901 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        String str=scanner.nextLine();
+        int num=0;
+
+        for(int i=1;i<str.length();i++){
+            if(str.charAt(i)=='-' || str.charAt(i)=='+' || str.charAt(i)=='*' ){
+                num++ ;
+            }
+        }
+        System.out.println(num);
+
+    }
 }
