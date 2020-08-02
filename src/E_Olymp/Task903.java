6a2d3233664fd3488bd8c8e6b0a6b90cde871373
Gunay-Jafar <cafarligunay06@gmail.com>
2020-08-02 15:59:10 +0400

    Created Task 903

---
+++ src/E_Olymp/Task903.java
@@ -0,0 +1,22 @@
+package E_Olymp;
+
+import java.util.Scanner;
+
+public class Task903 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int input=scanner.nextInt();
+
+        int first=input/100;
+
+        int last=input%10;
+
+        if (first>last)
+            System.out.println(first);
+        else if(last>first)
+            System.out.println(last);
+        else
+            System.out.println("=");
+
+    }
+}
