877d9395599360472c14179ce7ffb5e45b584997
Gunay-Jafar <cafarligunay06@gmail.com>
2020-08-09 00:52:03 +0400

    Created new tasks from E-Olimp

--- src/E_Olymp/Task930.java
+++ src/E_Olymp/Task930.java
@@ -1,24 +1,45 @@
 package E_Olymp;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
+//100
 public class Task930 {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        String num = scanner.nextLine();
+        ArrayList<Character> arrayList = new ArrayList<>();
+
+        HashMap<Character, Boolean> phoneNum = new HashMap<>();
+        phoneNum.put('0', false);
+        phoneNum.put('1', false);
+        phoneNum.put('2', false);
+        phoneNum.put('3', false);
+        phoneNum.put('4', false);
+        phoneNum.put('5', false);
+        phoneNum.put('6', false);
+        phoneNum.put('7', false);
+        phoneNum.put('8', false);
+        phoneNum.put('9', false);
 
 
-        int[] nums = {0,1,2,3,4,5,6,7,8,9};
-        int say=0;
+        for (int i = 0; i < num.length(); i++) {
+            phoneNum.put(num.charAt(i), true);
+        }
 
-        for (int i=0;i<nums.length;i++){
-            int a=scanner.nextInt();
-            if (a!=nums[i]){
-                say++;
+        for (Map.Entry<Character, Boolean> entry : phoneNum.entrySet()) {
+            if (!entry.getValue()) {
+                arrayList.add(entry.getKey());
             }
         }
-        System.out.println(say);
 
+        System.out.println(arrayList.size());
 
+        for (Character c: arrayList){
+            System.out.print(c+" ");
+        }
 
     }
 }
