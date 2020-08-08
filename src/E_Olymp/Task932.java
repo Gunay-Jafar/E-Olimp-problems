ed1231ac878bfbdc7982b20857a8df67f0f649ef
Gunay-Jafar <cafarligunay06@gmail.com>
2020-08-09 01:36:00 +0400

    Created new task from E-Olimp

--- src/E_Olymp/Task932.java
+++ src/E_Olymp/Task932.java
@@ -2,12 +2,18 @@ package E_Olymp;
 
 import java.util.Scanner;
 
+//100
 public class Task932 {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        int sahe=scanner.nextInt();
-        int a=scanner.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        double sahe = scanner.nextDouble();
+        double a = scanner.nextDouble();
 
+
+        double D = Math.pow(a, 2) + 8 * sahe;
+        double h = ((-a) + Math.sqrt(D)) / 2;
+
+        System.out.printf("%.2f", h);
 
 
     }
