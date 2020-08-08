33c3bf20e9121ec64f4bca04fe24c866d90ebf5e
Gunay-Jafar <cafarligunay06@gmail.com>
2020-08-09 01:18:49 +0400

    Created new task from E-Olimp

--- src/E_Olymp/Task931.java
+++ src/E_Olymp/Task931.java
@@ -2,15 +2,25 @@ package E_Olymp;
 
 import java.util.Scanner;
 
+//100
 public class Task931 {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
-        int a=scanner.nextInt();
+        int input=scanner.nextInt();
+        int qaliq;
         int hasil=1;
         int cem=0;
 
-        int qaliq=0;
+        while (input!=0){
+            qaliq=input%10;
+            hasil=hasil*qaliq;
+            cem=cem+qaliq;
+            input=input/10;
+        }
 
+        double qismet=(double)hasil/(double)cem;
+
+        System.out.printf("%.3f",qismet);
 
     }
 }
