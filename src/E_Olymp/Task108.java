409f1c7327de53bb85f4ed0d2d7109ea7bba2c14
Gunay-Jafar <cafarligunay06@gmail.com>
2020-09-07 16:05:18 +0400

    Added new task

---
+++ src/E_Olymp/Task108.java
@@ -0,0 +1,22 @@
+package E_Olymp;
+
+import java.util.Scanner;
+
+public class Task108 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int a=scanner.nextInt();
+        int b=scanner.nextInt();
+        int c=scanner.nextInt();
+
+        if ((a>b && a<c) ||(a<b && a>c))
+            System.out.println(a);
+        else if ((b>a && b<c) ||(b<a && b>c))
+            System.out.println(b);
+        else System.out.println(c);
+
+
+
+
+    }
+}
