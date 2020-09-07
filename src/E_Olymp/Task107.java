a7729bddf6de6ccece19a68733a4472b19d2d82e
Gunay-Jafar <cafarligunay06@gmail.com>
2020-09-07 15:54:43 +0400

    Added new task

---
+++ src/E_Olymp/Task107.java
@@ -0,0 +1,20 @@
+package E_Olymp;
+
+import java.util.Scanner;
+
+public class Task107 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+
+        int yuzlukSayi = input / 100;
+        int qaliq = input - yuzlukSayi * 100;
+
+        int iyirmilikSayi = qaliq / 20;
+        int qaliq2 = qaliq - iyirmilikSayi * 20;
+
+        System.out.println(yuzlukSayi * 100 + iyirmilikSayi * 30 + qaliq2 * 2);
+
+
+    }
+}
